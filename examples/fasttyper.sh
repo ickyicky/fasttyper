0551468a450f49e2bb1a16889cf0c773b0c2ba80
Doman <pi.domanski@gmail.com>
2021-07-13 19:36:11 +0200

    added example

---
+++ examples/fasttyper.sh
@@ -0,0 +1,10 @@
+# fasttyper
+function ff() {
+	mkdir -p ~/.cache/fasttyper
+	local amount="${1:-50}"
+	local language="${2:-english}"
+	local sfile=~/.cache/fasttyper/$language
+	local source_path=https://raw.githubusercontent.com/Miodec/monkeytype/master/static/languages/$language.json
+	[[ ! -f $sfile ]] && curl -s $source_path | python3 -c "import sys, json; print('\n'.join(json.load(sys.stdin)['words']))" > $sfile
+	shuf -n $amount $sfile | python3 -m fasttyper
+}
