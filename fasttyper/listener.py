afe232dd598f3449787bb1a00912546d20ba8690
Doman <domanski@basement.pl>
2022-03-18 14:33:13 +0100

    fix ą character...

--- fasttyper/listener.py
+++ fasttyper/listener.py
@@ -20,6 +20,18 @@ class Listener:
     def __init__(self, backspace_debug=False):
         self.backspace_debug = not backspace_debug
 
+    def action_for_char(self, key):
+        if key == "\t":
+            raise StoppingSignal(silent=True)
+
+        if ord(key) in (8, 127):
+            return self.action_for_key(ord(key))
+
+        if key in string.whitespace:
+            return Action.add_space
+
+        return Action.add_char
+
     def action_for_key(self, key):
         if (self.backspace_debug and key == 263) or (
             not self.backspace_debug and key == 127
@@ -37,17 +49,16 @@ class Listener:
         if key in WHITE:
             return Action.add_space
 
-        return Action.add_char
-
     def handle_key(self, key):
         action = Action.invalid
 
         if isinstance(key, str):
-            key = ord(key)
+            action = self.action_for_char(key)
+        else:
+            action = self.action_for_key(key)
+            key = chr(key)
 
-        action = self.action_for_key(key)
-
-        return action, chr(key)
+        return action, key
 
     def listen(self, screen):
         try:
