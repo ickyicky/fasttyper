135f38447c14e2fac4cbf551bd2e7ba827d42273
Doman <domanski@basement.pl>
2022-04-12 19:24:45 +0200

    changed color on reference text

--- fasttyper/config.py
+++ fasttyper/config.py
@@ -2,7 +2,7 @@ class Config:
     defaults = {
         "user_input_valid_color": 3,
         "user_input_invalid_color": 2,
-        "reference_text_color": 8,
+        "reference_text_color": 9,
         "stats_template": "wpm: {stats.wpm:0.2f}, time: {stats.total_seconds:0.2f}s",
         "stats_color": 5,
         "stats_position": "top",
